050f6f50f50150308d3ef47294a6e175335f4530
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-02-06 19:00:36 +0900

    add: シフト演算用のパーサー

--- PlusPim/Debuggers/PlusPimDbg/Instructions/RTypeInstruction.cs
+++ PlusPim/Debuggers/PlusPimDbg/Instructions/RTypeInstruction.cs
@@ -8,7 +8,10 @@ namespace PlusPim.Debuggers.PlusPimDbg.Instructions;
 /// </summary>
 internal abstract partial class RTypeInstruction: IInstruction {
     [GeneratedRegex(@"^\$(?<rd>\w+),\s*\$(?<rs>\w+),\s*\$(?<rt>\w+)$")]
-    private static partial Regex OperandsPattern();
+    private static partial Regex Operands3RegPattern();
+
+    [GeneratedRegex(@"^\$(?<rd>\w+),\s*\$(?<rs>\w+),\s*(?<shamt>\d+)$")]
+    private static partial Regex Operands2RegShamtPattern();
 
     // オペランドのレジスタID
     protected RegisterID Rd { get; }
@@ -22,6 +25,14 @@ internal abstract partial class RTypeInstruction: IInstruction {
         this.Rd = rd;
         this.Rs = rs;
         this.Rt = rt;
+        this.Shamt = new Immediate(-1); // 使用しない
+    }
+
+    protected RTypeInstruction(RegisterID rd, RegisterID rs, Immediate shamt) {
+        this.Rd = rd;
+        this.Rs = rs;
+        this.Rt = RegisterID.Zero; // 使用しない
+        this.Shamt = shamt;
     }
 
     public abstract void Execute(IExecutionContext context);
@@ -55,6 +66,41 @@ internal abstract partial class RTypeInstruction: IInstruction {
             rd = rdParsed;
             rs = rsParsed;
             rt = rtParsed;
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// オペランドを2レジスタとシフト量を指定している文字列から解析してRegisterIDと即値に変換する
+    /// </summary>
+    /// <param name="operands">対象の文字列</param>
+    /// <param name="rd"><see langword="true"/>ならばRegsiterIDが代入される．<see langword="false"/>のときの値は未定義．</param>
+    /// <param name="rs">rdと同様</param>
+    /// <param name="shamt">シフト量が代入される</param>
+    /// <returns><see langword="true"/>ならば解析成功</returns>
+    internal static bool TryParse3RegOperands(
+        string operands,
+        [MaybeNullWhen(false)] out RegisterID rd,
+        [MaybeNullWhen(false)] out RegisterID rs,
+        [MaybeNullWhen(false)] out Immediate shamt) {
+
+        rd = default;
+        rs = default;
+        shamt = default;
+
+        Match match = Operands2RegShamtPattern().Match(operands);
+        if(!match.Success) {
+            return false;
+        }
+
+        if(Enum.TryParse<RegisterID>(match.Groups["rd"].Value, true, out RegisterID rdParsed)
+            && Enum.TryParse<RegisterID>(match.Groups["rs"].Value, true, out RegisterID rsParsed)
+            && Immediate.TryParse(match.Groups["shamt"].Value, null, out Immediate? shamtParsed)) {
+            rd = rdParsed;
+            rs = rsParsed;
+            shamt = shamtParsed;
             return true;
         }
 
